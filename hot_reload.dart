51b4b13256c9d5f0cc9fc5d5aaf5cb66cb52b1d3
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-12 16:34:21 -0400

    Create hot_reload.dart

---
+++ hot_reload.dart
@@ -0,0 +1,12 @@
+// ...
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random AWESOME idea:'),  // ← Example change.
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+    );
+
+// ...
