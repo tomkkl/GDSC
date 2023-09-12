bd5c02a3779a086f9ee022d7be5cbeed94d20d7d
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-12 16:30:30 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -27,12 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
 }
 
 class MyHomePage extends StatelessWidget {
@@ -45,14 +39,6 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
-
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-
         ],
       ),
     );
