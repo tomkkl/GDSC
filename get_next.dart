4288cd7f4985cc96f4e6f6b648da6bdf617881c6
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-12 16:42:22 -0400

    Create get_next.dart

---
+++ get_next.dart
@@ -0,0 +1,13 @@
+// ...
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+// ...
