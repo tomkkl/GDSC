8b382bb8240b56e0b4eaa20f91e4e783adbbdb80
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-26 16:27:54 -0400

    Create OptionalChanges.dart

---
+++ OptionalChanges.dart
@@ -0,0 +1,29 @@
+// ...
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ...
