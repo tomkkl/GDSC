362f4c49c5b3bb3d549ff60469e216ba3b27d2a1
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-26 16:27:21 -0400

    Create CenterUI.dart

---
+++ CenterUI.dart
@@ -0,0 +1,27 @@
+// ...
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  // ← Add this.
+        children: [
+          Text('A random AWESOME idea:'),
+          BigCard(pair: pair),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ...
