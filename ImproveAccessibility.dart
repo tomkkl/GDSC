1e7dd8d4a86e0a26e2a0ac07dca20aad9fc2d4d5
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-26 16:26:39 -0400

    Create ImproveAccessibility.dart

---
+++ ImproveAccessibility.dart
@@ -0,0 +1,25 @@
+// ...
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        // ↓ Make the following change.
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+
+// ...
