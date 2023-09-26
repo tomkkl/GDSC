bac6fca4cfdd248639ba67b807911c4c48e44803
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-09-26 16:25:15 -0400

    Create TextTheme.dart

---
+++ TextTheme.dart
@@ -0,0 +1,21 @@
+// ...
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    // ↓ Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        // ↓ Change this line.
+        child: Text(pair.asLowerCase, style: style),
+      ),
+    );
+  }
+
+// ...
