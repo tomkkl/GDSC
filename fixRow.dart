708b6e0c0e6ee9d5c60ec37b03f8f6ff3036e4ba
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-10-03 18:48:44 -0400

    Update fixRow.dart

--- fixRow.dart
+++ fixRow.dart
@@ -1,1 +1,31 @@
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,   // ← Add this.
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
