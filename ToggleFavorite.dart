692bf4cf416671ebb06725f0e6db1ec4069d2fed
Tom Linn <70476250+tomkkl@users.noreply.github.com>
2023-10-03 18:43:25 -0400

    Create ToggleFavorite.dart

---
+++ ToggleFavorite.dart
@@ -0,0 +1,24 @@
+// ...
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  // ↓ Add the code below.
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+// ...
