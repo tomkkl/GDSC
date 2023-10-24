dd41864fa2266459d429a16fc5c634ad34eee03e
Tom Linn <tomkyawkhinelin@gmail.com>
2023-10-24 18:05:59 -0400

    Create useSelectedIndex.dart

---
+++ useSelectedIndex.dart
@@ -0,0 +1,12 @@
+// Place the following code at the top of _MyHomePageState's build method, just before return Scaffold
+Widget page;
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = Placeholder();
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
